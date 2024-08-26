ab03e574e965d7720d08ff7bcaea1259e1513c35
kingsantus <nkingsantus@gmail.com>
2024-08-26 14:58:33 +0100

    dummy data

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # rest_api_social_with_node
 A social media API
+
+## Install requirement of this project in package.json file
+## install nodemon with `npm install nodemon or npm i -g nodemon`
+## start app with `npx nodemon index.js` or `nodemon index.js`
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-#!/usr/bin/node
+#!/usr/bin/env node
 
 // importing express module
 const express = require('express');
@@ -11,8 +11,42 @@ const PORT = 5000;
 
 // creating a get route
 app.get("/", (req, res) => {
-    res.send("Hello Kingsantus!");
+    res.send("Hello Kingsantus welcome!!");
 });
+
+// creating dummy data
+const posts = [
+    {id:1, title:"first post", content:"this my first time"},
+    {id:2, title:"second post", content:"this my second post"},
+    {id:3, title:"third post", content:"this my third update"},
+]
+
+// sending dummy data with json
+app.get('/posts', (req, res) => {
+    res.json(posts);
+});
+
+// getting idividual id
+app.get('/posts/:id', (req, res) => {
+    // get the id after /posts
+    const postID = parseInt(req.params.id);
+    // interate and ensure the postID is found in post
+    const post=posts.find((p)=>p.id===postID);
+    // validation
+    if(!post){
+        return res.status(404).json({error:"Post not found"});
+    }
+    res.send(post);
+})
+
+// creating a post route
+app.post("/posts", (req, res) => {
+    const title = "more post";
+    const content = "This is another post";
+    const newPost = {id:posts.length+1, title, content}
+    posts.push(newPost);
+    res.status(201).json(newPost);
+})
 
 // server to listen
 app.listen(PORT, () => {
