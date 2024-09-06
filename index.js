5a4062adcc263e5836d48f2b5e3e33b6234b992c
kingsantus <nkingsantus@gmail.com>
2024-09-06 06:19:54 +0100

    verify jwt

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ const postRoute = require("./routes/posts");
 const commentRoute = require("./routes/comments");
 const storyRoute = require("./routes/stories");
 const conversationRoute = require("./routes/conversations");
+const messageRoute = require("./routes/messages");
 
 // importing path to allow it for middleware
 const path = require('path');
@@ -24,8 +25,9 @@ const path = require('path');
 // importing cookieParser to add the cookie value
 const cookieParser = require('cookie-parser');
 
-// importing error handler as the middleware
+// importing error handler, verify token as the middleware
 const { errorHandler, CustomError } = require("./middlewares/error");
+const verifyToken = require('./middlewares/verifyToken');
 
 // initializing it to config
 dotenv.config();
@@ -40,11 +42,12 @@ app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 // instatiating the routes
 app.use("/api/auth", authRoute);
-app.use("/api/user", userRoute);
-app.use("/api/post", postRoute);
-app.use("/api/comment", commentRoute);
-app.use("/api/story", storyRoute);
-app.use("/api/conversation", conversationRoute);
+app.use("/api/user",verifyToken, userRoute);
+app.use("/api/post",verifyToken, postRoute);
+app.use("/api/comment", verifyToken, commentRoute);
+app.use("/api/story", verifyToken, storyRoute);
+app.use("/api/conversation", verifyToken, conversationRoute);
+app.use("/api/message", verifyToken, messageRoute);
 
 // instantiationg errorHandler to app
 app.use(errorHandler);
